335cae918cad72da497663a513bc3b3264a398d7
Sanglim <narrrrrmm00@gmail.com>
2024-04-30 01:17:58 +0900

    [level 1] Title: 달리기 경주, Time: 0.17 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/1/178871. 달리기 경주/달리기 경주.js
@@ -0,0 +1,21 @@
+function solution(players, callings) {
+    
+    const myMap = new Map();
+    
+    players.forEach((item, idx) => {
+        myMap.set(item, idx);
+    })
+
+    callings.forEach((item, idx2) => {
+        let idx = myMap.get(item);
+
+        let temp = players[idx -1];
+        players[idx-1] = players[idx];
+        players[idx] = temp;
+        
+        myMap.set(item, idx-1);
+        myMap.set(temp, idx);
+    })
+    
+    return players;
+}
