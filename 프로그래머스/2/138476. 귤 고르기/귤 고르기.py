8a7e73aaa09ee71b2c18cdb1ac25121699b74e4f
Sanglim <narrrrrmm00@gmail.com>
2024-05-19 01:33:02 +0900

    [level 2] Title: 귤 고르기, Time: 22.73 ms, Memory: 18 MB -BaekjoonHub

---
+++ 프로그래머스/2/138476. 귤 고르기/귤 고르기.py
@@ -0,0 +1,21 @@
+def solution(k, tangerine):
+    answer = 0
+    
+    myDict = {}
+    for i in tangerine:
+        if i not in myDict:
+            myDict[i] = 1
+        else:
+            myDict[i] += 1
+    
+    arr = sorted(list(map(int, myDict.values())))
+    
+    _sum = 0
+    idx = len(arr) - 1
+
+    while _sum < k :
+        _sum += arr[idx]
+        idx -=1
+        answer += 1
+    
+    return answer
