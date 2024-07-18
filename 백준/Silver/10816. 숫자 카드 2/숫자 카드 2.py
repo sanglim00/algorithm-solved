a3b9fd7f07bc16fe6aa7cf61c84630996ce4d51c
Sanglim <narrrrrmm00@gmail.com>
2024-07-18 10:54:25 +0900

    [Silver IV] Title: 숫자 카드 2, Time: 836 ms, Memory: 137160 KB -BaekjoonHub

---
+++ 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.py
@@ -0,0 +1,21 @@
+import sys
+
+N = int(sys.stdin.readline())
+n_lst = list(map(int, sys.stdin.readline().split()))
+
+myDict = {}
+for i in n_lst:
+    if i in myDict:
+        myDict[i] += 1
+    else:
+        myDict[i] = 1
+
+M = int(sys.stdin.readline())
+m_lst = list(map(int, sys.stdin.readline().split()))
+
+for i in m_lst:
+    if i in myDict:
+        print(myDict[i], end=' ')
+    else:
+        print(0, end=' ')
+print()
