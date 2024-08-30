6878dbc791e2af495ca729b324a7f864dbc45c6c
Sanglim <narrrrrmm00@gmail.com>
2024-08-30 23:44:26 +0900

    [Bronze I] Title: 명령 프롬프트, Time: 36 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/1032. 명령 프롬프트/명령 프롬프트.py
+++ 백준/Bronze/1032. 명령 프롬프트/명령 프롬프트.py
@@ -1,12 +1,12 @@
-<html>
-<head><title>504 Gateway Time-out</title></head>
-<body>
-<center><h1>504 Gateway Time-out</h1></center>
-</body>
-</html>
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
+import sys
+
+N = int(sys.stdin.readline())
+str1 = sys.stdin.readline().rstrip()
+
+for _ in range(N-1):
+    str2 = sys.stdin.readline().rstrip()
+    for i in range(len(str2)):
+        if str1[i] != str2[i]: 
+            str1 = str1[:i] + "?" +str1[i+1:]
+
+print(str1)
