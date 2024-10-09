7911275250382ca1d89ca51f771b423f02433deb
Sanglim <narrrrrmm00@gmail.com>
2024-10-09 16:10:57 +0900

    [Silver I] Title: 구간 합 구하기 5, Time: 1076 ms, Memory: 149344 KB -BaekjoonHub

---
+++ 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.py
@@ -0,0 +1,33 @@
+import sys
+
+N, M = map(int, sys.stdin.readline().split())
+
+graph_ = []
+graph_.append([0 for _ in range(N)])
+for _ in range(N):
+    lst = list(map(int, sys.stdin.readline().split()))
+    graph_.append(lst)
+
+sum_ = [[0 for _ in range(N+1)] for _ in range(N+1)]
+for i in range(1, N+1): sum_[i][1] = graph_[i][0]
+
+for i in range(1, N+1):
+    for j in range(1, N+1):
+        sum_[i][j] = sum_[i][j-1]+graph_[i][j-1]
+
+dp = [[0 for _ in range(N+1)] for _ in range(N+1)]
+for i in range(1, N+1): dp[1][i] = sum_[1][i]
+
+for i in range(2, N+1):
+    for j in range(1, N+1):
+        dp[i][j] = dp[i-1][j]+sum_[i][j]
+
+for _ in range(M):
+    ans = 0
+    x1, y1, x2, y2 = map(int, sys.stdin.readline().split())
+
+    if y1 ==1 and x1 ==1 and y2==N and x2==N: 
+        print(dp[-1][-1])
+    else:
+        ans = dp[x2][y2] - (dp[x2][y1-1] + dp[x1-1][y2]) + dp[x1-1][y1-1]
+        print(ans)
