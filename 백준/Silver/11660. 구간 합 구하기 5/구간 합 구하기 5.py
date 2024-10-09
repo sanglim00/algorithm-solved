7902e4cea397b9bdda5ba09bf9c5289111ed43f9
Sanglim <narrrrrmm00@gmail.com>
2024-10-09 16:13:49 +0900

    Update 구간 합 구하기 5.py

--- 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.py
+++ 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.py
@@ -2,12 +2,14 @@ import sys
 
 N, M = map(int, sys.stdin.readline().split())
 
+# 입력값 저장
 graph_ = []
 graph_.append([0 for _ in range(N)])
 for _ in range(N):
     lst = list(map(int, sys.stdin.readline().split()))
     graph_.append(lst)
 
+# 1차원 누적합 계산
 sum_ = [[0 for _ in range(N+1)] for _ in range(N+1)]
 for i in range(1, N+1): sum_[i][1] = graph_[i][0]
 
@@ -15,6 +17,7 @@ for i in range(1, N+1):
     for j in range(1, N+1):
         sum_[i][j] = sum_[i][j-1]+graph_[i][j-1]
 
+# 2차원 누적합 계산
 dp = [[0 for _ in range(N+1)] for _ in range(N+1)]
 for i in range(1, N+1): dp[1][i] = sum_[1][i]
 
@@ -22,8 +25,8 @@ for i in range(2, N+1):
     for j in range(1, N+1):
         dp[i][j] = dp[i-1][j]+sum_[i][j]
 
+# 계산
 for _ in range(M):
-    ans = 0
     x1, y1, x2, y2 = map(int, sys.stdin.readline().split())
 
     if y1 ==1 and x1 ==1 and y2==N and x2==N: 
