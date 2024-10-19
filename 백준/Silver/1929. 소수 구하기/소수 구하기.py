7bf9a609168f3c5e1101585afaa67fd3fc932e00
Sanglim <narrrrrmm00@gmail.com>
2024-10-19 20:46:06 +0900

    [Silver III] Title: 소수 구하기, Time: 468 ms, Memory: 70088 KB -BaekjoonHub

---
+++ 백준/Silver/1929. 소수 구하기/소수 구하기.py
@@ -0,0 +1,14 @@
+M, N = map(int, input().split())
+lst = [i for i in range(N+1)]
+lst[0] = 0
+lst[1] = 0
+
+ans = []
+
+for i in range(2, N+1):
+    if lst[i]:
+        for j in range(2*i, N+1, i):
+            lst[j] = 0
+
+for i in range(M, N+1):
+    if lst[i]: print(lst[i])
