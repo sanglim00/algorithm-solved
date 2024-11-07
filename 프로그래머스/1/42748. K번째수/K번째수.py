c8548240b1c7f53323207b5c96dfa851bcba8bec
Sanglim <narrrrrmm00@gmail.com>
2024-11-07 23:47:33 +0900

    [level 1] Title: K번째수, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/1/42748. K번째수/K번째수.py
+++ 프로그래머스/1/42748. K번째수/K번째수.py
@@ -1,2 +1,6 @@
 def solution(array, commands):
-     return list(map(lambda x:sorted(array[x[0]-1:x[1]])[x[2]-1], commands))
+    answer = []
+    for i, j, k in commands:
+        arr = array[i-1:j]
+        answer.append(sorted(arr)[k-1])
+    return answer
