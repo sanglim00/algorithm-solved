09c5e2f3036eed182881c9200402766966efdc76
Sanglim <narrrrrmm00@gmail.com>
2024-11-12 16:19:52 +0900

    [Gold III] Title: 택배, Time: 2892 ms, Memory: 48540 KB -BaekjoonHub

---
+++ 백준/Gold/1719. 택배/택배.py
@@ -0,0 +1,38 @@
+import sys
+import heapq
+
+N, M = map(int, sys.stdin.readline().split())
+
+graph = [[] for _ in range(N+1)]
+for _ in range(M):
+    a, b, c = map(int, sys.stdin.readline().split())
+    graph[a].append((b, c))
+    graph[b].append((a, c))
+
+dist = [[(-1, float('INF')) for _ in range(N+1)] for _ in range(N+1)]
+
+def dijkstra(i, j):
+    heap = [(0, i, j)]
+    dist[i][j] = (0, 0)
+
+    while heap:
+        now_cost, now_y, now_x = heapq.heappop(heap)
+
+        if dist[now_y][now_x][1] < now_cost: continue
+
+        for next_x, cost in graph[now_y]:
+            all_cost = now_cost + cost
+
+            if dist[now_x][next_x][1] > all_cost:
+                if now_y==now_x: dist[now_x][next_x] = (now_y, all_cost)
+                dist[now_x][next_x] = (now_y, all_cost)
+                heapq.heappush(heap, (all_cost, now_x, next_x))
+    
+for i in range(1, N+1):
+    dijkstra(i, i)
+
+for i in range(1, N+1):
+    for j in range(1, N+1):
+        print(dist[j][i][0] if i != j else '-', end=' ')
+    print()
+print()
