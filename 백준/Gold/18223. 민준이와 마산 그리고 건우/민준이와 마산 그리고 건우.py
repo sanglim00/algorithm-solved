d2604ba9f99ab7ee41ff002409a7127711c2339a
Sanglim <narrrrrmm00@gmail.com>
2024-11-15 14:14:50 +0900

    [Gold IV] Title: 민준이와 마산 그리고 건우, Time: 72 ms, Memory: 36260 KB -BaekjoonHub

--- 백준/Gold/18223. 민준이와 마산 그리고 건우/민준이와 마산 그리고 건우.py
+++ 백준/Gold/18223. 민준이와 마산 그리고 건우/민준이와 마산 그리고 건우.py
@@ -2,6 +2,7 @@ import sys
 import heapq
 
 V, E, P = map(int, sys.stdin.readline().split())
+
 graph = [[] for _ in range(V+1)]
 for _ in range(E):
     A, B, C = map(int, sys.stdin.readline().split())
@@ -10,18 +11,18 @@ for _ in range(E):
 
 def dijkstra(num):
     heap = [(0, num)]
-    dist = [(-1, -1, float("INF")) for _ in range(V+1)]
-    dist[num] = (0, 0, 0)
+    dist = [float("INF") for _ in range(V+1)]
+    dist[num] = 0
 
     while heap:
         now_cost, now_line = heapq.heappop(heap)
-        if dist[now_line][2] < now_cost: continue
+        if dist[now_line] < now_cost: continue
 
         for next, cost in graph[now_line]:
             all_cost = now_cost + cost
 
-            if dist[next][2] >= all_cost:
-                dist[next] = (now_line, next, all_cost)
+            if dist[next] >= all_cost:
+                dist[next] = all_cost
                 heapq.heappush(heap, (all_cost, next))
 
     return dist
@@ -29,7 +30,7 @@ def dijkstra(num):
 a = dijkstra(1) 
 b = dijkstra(P)
 
-ans1 = a[V][2]
-ans2 = a[P][2] + b[V][2]
+ans1 = a[V]
+ans2 = a[P] + b[V]
 
 print("SAVE HIM" if ans1 == ans2 else "GOOD BYE")
