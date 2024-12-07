31a1657c9d4c1ac861fa5645306166ca8d881a62
Sanglim <narrrrrmm00@gmail.com>
2024-12-07 23:10:58 +0900

    [Bronze V] Title: 진흥원 세미나, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/30087. 진흥원 세미나/진흥원 세미나.py
@@ -0,0 +1,13 @@
+myDict = {
+    'Algorithm': '204',
+    'DataAnalysis': '207',
+    'ArtificialIntelligence': '302',
+    'CyberSecurity': 'B101',
+    'Network': '303',
+    'Startup': '501',
+    'TestStrategy': '105'
+}
+N = int(input())
+for _ in range(N):
+    str_ = input().rstrip()
+    print(myDict[str_])
